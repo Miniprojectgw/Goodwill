4164d1fda3653168009e296bb23711a0f7541659
Ivine Joju <110597449+ivinej@users.noreply.github.com>
2023-07-31 11:05:52 +0530

    navbar wroking

--- client/src/Components/Nav2.js
+++ client/src/Components/Nav2.js
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom';
 import { NavLink, Outlet } from 'react-router-dom'
 
 const Nav2 = () => {
+
+  const handleClick =()=>{
+    window.location.reload()
+  }
   return (
     <div> 
     <div className='ml-4 mr-4'>
     <nav className="bg-white py-4 px-6 flex items-center justify-between w-full">
       <ul className="flex justify-between items-center text-black w-full">
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/Studhome/feed">Feed</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/Studhome/sponsorships">Sponsorships</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/Studhome/internships">Internships</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/chat">Chat</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/contact">Contact</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/Studhome/feed">Feed</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/Studhome/sponsorships">Sponsorships</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/Studhome/internships">Internships</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/chat">Chat</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
     </div>
--- client/src/Components/Nav3.js
+++ client/src/Components/Nav3.js
@@ -3,16 +3,23 @@ import { Link } from 'react-router-dom';
 import { NavLink, Outlet } from 'react-router-dom'
 
 const Nav3 = () => {
+
+  
+
+
+  const handleClick =()=>{
+    window.location.reload()
+  }
   return (
     <div> 
     <div className='ml-4 mr-4'>
     <nav className="bg-white py-4 px-6 flex items-center justify-between w-full">
       <ul className="flex justify-between items-center text-black w-full">
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/Alumhome/feed">Feed</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/Alumhome/sponsorships">Sponsorships</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/Alumhome/internships">Internships</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/chat">Chat</Link></li>
-        <li className="hover:text-blue-200 cursor-pointer"><Link to="/contact">Contact</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/Alumhome/feed">Feed</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/Alumhome/sponsorships">Sponsorships</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/Alumhome/internships">Internships</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/chat">Chat</Link></li>
+        <li className="hover:text-blue-200 cursor-pointer" onClick={handleClick}><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
     </div>
